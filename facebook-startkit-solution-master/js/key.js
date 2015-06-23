4122b23b2284d77e645aded1104f92df923953c6
rockmanx41024 <rockmanx41024@yahoo.com.tw>
2015-06-24 02:24:16 +0800

    fff

--- facebook-startkit-solution-master/js/key.js
+++ facebook-startkit-solution-master/js/key.js
@@ -1,20 +1,20 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
 
-window.fbAsyncInit = function() {
-	FB.init({
-		      appId      : '1616729421906225',
-		      xfbml      : true,
-		      version    : 'v2.3'
-		    });
-	};
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1652401031642744',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
 
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
-		fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 
