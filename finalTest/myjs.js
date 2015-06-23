d72f611bc79528f11dbbc97bb0e2ef40f2a2f1b1
rockmanx41024 <rockmanx41024@yahoo.com.tw>
2015-06-23 21:02:27 +0800

    fff

--- finalTest/myjs.js
+++ finalTest/myjs.js
@@ -15,6 +15,13 @@ window.fbAsyncInit = function() {
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
   
+$body = $("body");
+
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+     ajaxStop: function() { $body.removeClass("loading"); }    
+});
+ 
 $(document).ready(function(){
 
    /* $('#quote').flipQuote({
